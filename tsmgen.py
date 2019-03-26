0f0f5e6622a4b4ac1a7fe657172ae047c7b4d06d
Dominik Ernst <dominik.ernst@fau.de>
2019-03-26 12:32:52 +0100

    factor out predict.py

--- tsmgen.py
+++ tsmgen.py
@@ -1,6 +1,7 @@
 #!/bin/python
 
 from kernel import Kernel
+from predict import *
 import numpy as np
 import pycuda.driver as drv
 import pycuda
@@ -70,46 +71,7 @@ def benchKernel(kernel, K):
 #            print( str(MN) + "  " + str(TM) + " " + str(TN) + ":  ", end="")
 
 
-def predictL1(kernel):
-    warpAddresses = [kernel.genAddresses(warpLane) for warpLane in range(0, 32)]
 
-    L1CLs = [
-        set([warpAddresses[warpLane][load][1] // 128 for warpLane in range(0, 32)])
-        for load in range(len(warpAddresses[0]))
-    ]
-
-    L1cycles = sum([max(2, len(load)) for load in L1CLs])
-
-    return kernel.TM * kernel.TN / max(L1cycles, kernel.TM * kernel.TN) * 1.38 * 2 * 32 * 80
-
-
-def predictMemory(kernel):
-    addresses = [[(l[0], l[1] // 64) for l in kernel.genAddresses(warpLane)]
-                 for warpLane in range(0, kernel.blockSize)]
-
-    CLs = set([
-        addresses[warpLane][load] for warpLane in range(0, kernel.blockSize)
-        for load in range(len(addresses[0]))
-    ])
-
-    estRegCount = min(256, (kernel.TM * kernel.TN + kernel.TM + kernel.TN) * 2 + 10)
-    estBlockCount = kernel.estimateBlockCount(estRegCount)
-
-    aluInstrPerLoop = 10
-    instrExecLatency = max(aluInstrPerLoop * 2,
-                           aluInstrPerLoop * estBlockCount * (kernel.blockSize / 32) / 2)
-
-    latencyPred = 80 * estBlockCount * kernel.TM * kernel.TN * kernel.blockSize * 2 * (
-        1.38 / (436 + kernel.TN * kernel.TM * 4 + (kernel.TM + kernel.TN) * 2 + instrExecLatency))
-
-    throughputPred = 2 * kernel.TM * kernel.TN * kernel.blockSize / (len(CLs) * 64 / 880e9) / 1e9
-    return (latencyPred, throughputPred)
-
-
-def predictALU(kernel):
-    aluInstrPerLoop = 10
-    GIts = 1.38 * 80 * 2 * 32 / aluInstrPerLoop
-    return GIts * kernel.TM * kernel.TN
 
 
 kernels = [
